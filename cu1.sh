bb3d6cf798885c5ac1d4bdacf56d40c51e6c8a8c
sonudurwaskatkar <sonukatkar96@gmail.com>
2020-05-18 18:51:37 +0530

    katkar

--- cu1.sh
+++ cu1.sh
@@ -1,1 +1,11 @@
-echo "wellcome......"
+#!/bin/bash -x
+isPresent=1;
+isAbsent=0;
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "present"
+else
+        echo "Absent"
+fi
